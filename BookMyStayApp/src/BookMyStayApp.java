4af7943160357bcbe9202559040cecf1b661b632
Charan-3132 <np3458@srmist.edu.in>
2026-03-19 22:19:33 +0530

    UC2: Basic room types with static availability

--- BookMyStayApp/src/BookMyStayApp.java
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -1,30 +1,89 @@
 /**
  * BookMyStayApp
  *
- * This class represents the entry point of the BookMyStay Hotel Booking application.
- * It demonstrates how a Java program starts execution and displays
- * a welcome message to the user.
+ * This class demonstrates basic room modeling using abstraction,
+ * inheritance, and static availability representation.
  *
- * @author Vaishnavi
+ * @author Charan
  * @version 1.0
  */
+
+// Abstract class
+abstract class Room {
+
+    protected String type;
+    protected int beds;
+    protected double price;
+
+    // Constructor
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    // Method to display details
+    public void displayDetails() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: ₹" + price);
+    }
+}
+
+// Single Room
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 1500);
+    }
+}
+
+// Double Room
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 2500);
+    }
+}
+
+// Suite Room
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 5000);
+    }
+}
+
+// Main Application Class
 public class BookMyStayApp {
 
-    /**
-     * Main method - Entry point of the application
-     * JVM starts execution from here
-     */
     public static void main(String[] args) {
 
-        // Welcome message
-        System.out.println("Welcome to BookMyStay");
+        System.out.println("Welcome to BookMyStay - Room Availability");
 
-        // Application name and version
-        System.out.println("Application: BookMyStay Hotel Booking System");
-        System.out.println("Version: 1.0");
+        // Create room objects (Polymorphism)
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
-        // End message
-        System.out.println("Application Started Successfully");
+        // Static availability variables
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
 
+        // Display details
+        System.out.println("\n--- Room Details ---");
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailable);
+
+        System.out.println();
+
+        doubleRoom.displayDetails();
+        System.out.println("Available: " + doubleAvailable);
+
+        System.out.println();
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailable);
+
+        System.out.println("\nApplication Terminated");
     }
 }
